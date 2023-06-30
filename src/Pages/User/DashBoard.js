e73ceb6c990e084f65390853cd9624e1dba404af
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-06-30 11:15:22 +0530

    Update DashBoard.js

--- src/Pages/User/DashBoard.js
+++ src/Pages/User/DashBoard.js
@@ -2,25 +2,8 @@ import React, { useState } from "react";
 import Card from "../../Ui/Card";
 import classes from "./DashBoard.module.css";
 import myImage from "../../Images/dummyuser.jpg";
-import { useEffect } from "react";
-import axios from "axios";
 
 const DashBoard = () => {
-  const [name,setName] = useState("");
-  const [username, setusername] = useState("");
-  const [mobile, setMobile] = useState("");
-  const [email, setEmail] = useState("");
-  useEffect(()=>{
-    const fetchData = async () => {
-      const response = await axios.get('https://librarymanagement-fqqg.onrender.com/dashboard');
-      console.log(response);
-      setusername(response.data.name.username);
-      setEmail(response.data.name.email);
-      setName(response.data.name.name);
-      setMobile(response.data.name.mobileno);
-    };
-    fetchData();
-  },[])
   return (
     <Card>
       <h2>Welcome {username}</h2>
@@ -29,16 +12,16 @@ const DashBoard = () => {
       </div>
       <div className={classes.dashboardcontent}>
         <div>
-          <h3>{username}</h3>
+          <h3>username</h3>
         </div>
         <div>
-          <h3>{email}</h3>
+          <h3>email</h3>
         </div>
         <div>
-          <h3>{name}</h3>
+          <h3>name</h3>
         </div>
         <div>
-          <h3>{mobile}</h3>
+          <h3>mobile</h3>
         </div>
       </div>
     </Card>
